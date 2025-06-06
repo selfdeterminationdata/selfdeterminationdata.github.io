01eaa1a4846633de64b6ecab7b29d0a90238f254
george-ai-ob <george.ai.obrien@gmail.com>
2025-06-06 18:01:41 +0100

    skeleton page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import "@/styles/map.css";
+
 export default function Home() {
   return (
-    <></>
+    <>
+    <h1>
+      Research
+    </h1>
+    <div className="map">
+
+    </div>
+    </>
   );
 }
