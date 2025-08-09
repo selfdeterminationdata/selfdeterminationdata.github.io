349cfe178d54cb5f1f4900aea358f1b6a58c5605
swathi <swathipravin012@gmail.com>
2025-08-10 00:26:37 +0100

    webpack changes

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 const ESLintPlugin = require("eslint-webpack-plugin");
 const { TsconfigPathsPlugin } = require("tsconfig-paths-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const packageJson = require("./package.json");
 
 module.exports = (env) => ({
@@ -16,6 +17,8 @@ module.exports = (env) => ({
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "build.js",
+    publicPath: "/", // ensures assets load correctly
+    clean: true, // clears dist/ before build
   },
   module: {
     rules: [
@@ -31,18 +34,43 @@ module.exports = (env) => ({
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "static/media/[name][ext]", // keep original filenames
+        },
+      },
     ],
   },
   plugins: [
+    // Generates index.html with correct JS bundle injected
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
+
+    // Copies all public/ files except index.html into dist/
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "public"),
+          to: path.resolve(__dirname, "dist"),
+          globOptions: {
+            ignore: ["**/index.html"], // don't copy HTML twice
+          },
+        },
+      ],
+    }),
+
+    // Define env vars available in client-side code
     new webpack.DefinePlugin({
-      "process.env.PRODUCTION": env.production || !env.development,
+      "process.env.PRODUCTION": JSON.stringify(env.production || !env.development),
       "process.env.NAME": JSON.stringify(packageJson.name),
       "process.env.VERSION": JSON.stringify(packageJson.version),
     }),
+
     new ForkTsCheckerWebpackPlugin(),
     new ESLintPlugin({ files: "./src/**/*.{ts,tsx,js,jsx}" }),
   ],
 });
+
