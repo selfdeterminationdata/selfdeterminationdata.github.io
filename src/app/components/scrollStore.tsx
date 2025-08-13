46749a8d7ca5f27966502a8b7b66216b05985a3f
swathi <swathipravin012@gmail.com>
2025-08-13 20:43:57 +0400

    lint fix

--- src/app/components/scrollStore.tsx
+++ src/app/components/scrollStore.tsx
@@ -1,15 +1,13 @@
-import { create } from "zustand";
-
+import {create} from "zustand";
 
 type ScrollState = {
-  scrollLeft: number;
-  lastSource?: string;
-  setScrollLeft: (scrollLeft: number, sourceId?: string) => void;
+    scrollLeft: number;
+    lastSource?: string;
+    setScrollLeft: (scrollLeft: number, sourceId?: string) => void;
 };
 
 export const useScrollStore = create<ScrollState>((set) => ({
-  scrollLeft: 0,
-  lastSource: undefined,
-  setScrollLeft: (scrollLeft, sourceId) =>
-    set({ scrollLeft, lastSource: sourceId }),
+    scrollLeft: 0,
+    lastSource: undefined,
+    setScrollLeft: (scrollLeft, sourceId) => set({scrollLeft, lastSource: sourceId})
 }));
